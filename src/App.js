0675eeb90d497ca66b6c667ac7732b11243e6486
Axel Cisneros <axel.cisneros25@hotmail.com>
2023-03-18 01:59:34 -0600

    Codigo modificado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hola Mundo!!!!!!</h1>
-      </header>
-    </div>
-  );
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+    const element = (
+      <div className="App">
+        <header className="App-header">
+          <h1>La hora es:</h1>
+          <h2>En México: {new Date().toLocaleTimeString("es-MX")}</h2>
+          <p>Hola Mundo!!!!!!</p>
+        </header>
+      </div>
+    );
+
+  root.render(element)
 }
 
 export default App;
